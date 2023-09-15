627b01726d5669d2a8bfd7aa0ef68d3186a0c56f
henryhull <104516548+Astricklin3@users.noreply.github.com>
2023-09-15 10:23:59 -0500

    4.6 code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercise_3_3.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class Exercise_3_3 extends OpMode {
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Diff left y and right y",
+                gamepad1.left_stick_y - gamepad1.right_stick_y);
+        telemetry.addData("sum triggers",
+                gamepad1.left_trigger + gamepad1.right_trigger);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablesAndGamepads_OF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablesAndGamepads_OF.java
@@ -1,27 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+
 @TeleOp()
-public class VariablesAndGamepads extends OpMode {
+public class VariablesAndGamepads_OF extends OpMode {
+
+
+
+
     @Override
     public void init() {
-        FtcDashboard dashboard = FtcDashboard.getInstance();
-        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
         int teamNumber = 16072;
         double motorSpeed = 0.5;
         boolean touchSensorPressed = true;
-
         telemetry.addData("Team Number", teamNumber);
         telemetry.addData("Motor Speed", motorSpeed);
         telemetry.addData("Touch Sensor", touchSensorPressed);
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
+
     }
 
-    @Override
-    public void loop() {
+
+
+
+    public void loop(){
+
 
     }
 }
+
