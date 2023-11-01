536e41c11480561d6d0a8b2a6f6fb27d4fc7c22c
henryhull <104516548+Astricklin3@users.noreply.github.com>
2023-11-01 11:29:14 -0500

    Update Cool_Bot2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cool_Bot2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cool_Bot2.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -45,26 +16,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @TeleOp(name="Cool_Bot2")
 @Config
 //@Disabled
 public class Cool_Bot2 extends LinearOpMode {
 
-    // Declare OpMode members.
+    //Declare OpMode Members
+        //Private
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
@@ -73,6 +31,7 @@ public class Cool_Bot2 extends LinearOpMode {
     private Servo rightServo = null;
     private ColorSensor colorSensor;
     private DistanceSensor distanceSensor;
+        //Public
     public static int rightClaw_open = 70;
     public static int rightClaw_close = -40;
     public static int leftClaw_open = -70;
@@ -80,76 +39,65 @@ public class Cool_Bot2 extends LinearOpMode {
     public static int armUp = -650;
     public static int armDown = -100;
     public static int object = 5;
+    public static double speed = 1.0;
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
+        //Hardware Map
+            //Motors
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
         armMotor = hardwareMap.get(DcMotorEx.class, "arm_motor");
+            //Servos
         leftServo = hardwareMap.get(Servo.class, "left_servo");
         rightServo = hardwareMap.get(Servo.class, "right_servo");
+            //Sensors
         colorSensor = hardwareMap.get(ColorSensor.class,"color_sensor");
         distanceSensor = hardwareMap.get(DistanceSensor.class, "distance_sensor");
 
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
 
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        //Drive Directions
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
-        // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
 
-        //Stops/Resets Encoder
+        //Stops and Resets Encoder
         armMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
 
         waitForStart();
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Setup a variable for each drive wheel to save power level for telemetry
+            //Driving Variables
+                //Power
             double leftPower;
             double rightPower;
-
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
+                //Movement
             double turn = -gamepad1.right_stick_x;
             double drive = gamepad1.left_stick_x;
-            leftPower = Range.clip(drive + turn, -0.3, 0.3);
-            rightPower = Range.clip(drive - turn, -0.3, 0.3);
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
+            leftPower = Range.clip(drive + turn, -speed, speed);
+            rightPower = Range.clip(drive - turn, -speed, speed);
 
-            // Send calculated power to wheels
+            //Sends Power to Wheels
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
 
-            //Arm Code using encoder
+            //Arm Lift to Position (Using Encoder)
             if (gamepad1.dpad_up) {
-                // Set the motor's target position to 300 ticks
                 armMotor.setTargetPosition(armUp);
-                // Switch to RUN_TO_POSITION mode
                 armMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-                // Start the motor moving by setting the max velocity to 200 ticks per second
                 armMotor.setVelocity(200);
             }
+
+            //Arm Lower to Position (Using Encoder)
             if (gamepad1.dpad_down) {
                 // Set the motor's target position to 300 ticks
                 armMotor.setTargetPosition(armDown);
@@ -159,7 +107,7 @@ public class Cool_Bot2 extends LinearOpMode {
                 armMotor.setVelocity(200);
             }
 
-            //Up 10 Button
+            //Arm Lift Position
             if(gamepad1.b) {
                 int temp = armMotor.getCurrentPosition();
                 int newPosition = temp - 10;
@@ -168,7 +116,7 @@ public class Cool_Bot2 extends LinearOpMode {
                 armMotor.setPower(1);
             }
 
-            //Down 10 Button
+            //Arm Lower Position
             if(gamepad1.x) {
                 int temp = armMotor.getCurrentPosition();
                 int newPosition = temp + 10;
@@ -178,33 +126,45 @@ public class Cool_Bot2 extends LinearOpMode {
             }
 
             //Claw Code
+                //Claw Open
             if (gamepad1.left_bumper) {
                 rightServo.setPosition(leftClaw_open);
                 leftServo.setPosition(rightClaw_open);
             }
+                //Claw Close
             if (gamepad1.right_bumper) {
                 rightServo.setPosition(leftClaw_close);
                 leftServo.setPosition(rightClaw_close);
             }
 
-            //Distance Sensor Code
+            //Distance Sensor Claw Close
             if(gamepad1.y) {
                 if (distanceSensor.getDistance(DistanceUnit.INCH) < object) {
                     rightServo.setPosition(leftClaw_close);
                     leftServo.setPosition(rightClaw_close);
                 }
             }
-            //else {
-            //    rightServo.setPosition(leftClaw_open);
-            //    leftServo.setPosition(rightClaw_open);
-            //}
 
+            //Drive and Turn Speed
+                //Speed Max
+            if(gamepad1.left_stick_button){
+                    speed = 1.00;
+                }
+                //Speed Middle
+            if(gamepad1.a) {
+                    speed = 0.50;
+                }
+                //Speed Min
+            if(gamepad1.right_stick_button){
+                    speed = 0.25;
+                }
+            }
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            telemetry.addData("Status", "Waiting for the motor to reach its target");
+            //Telemetry
+            //(delete) telemetry.addData("Status", "Run Time: " + runtime.toString());
+            //(2 errors on this) telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.addData("Arm Position: ", armMotor.getCurrentPosition());
+            telemetry.addData("Driving/Turning Power: ", speed);
             telemetry.addData("Object Distance: ", distanceSensor.getDistance(DistanceUnit.INCH));
             telemetry.addData("Red: ", colorSensor.red());
             telemetry.addData("Green: ", colorSensor.green());
@@ -212,4 +172,3 @@ public class Cool_Bot2 extends LinearOpMode {
             telemetry.update();
         }
     }
-}
