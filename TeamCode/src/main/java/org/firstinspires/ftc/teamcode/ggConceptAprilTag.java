08e21da9cfebc4b4c470ab23a059351433e105b0
henryhull <104516548+Astricklin3@users.noreply.github.com>
2023-12-04 11:12:05 -0600

    v

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ggConceptAprilTag.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@TeleOp(name = "ggConceptAprilTag")
+@Disabled
+public class ggConceptAprilTag extends LinearOpMode {
+    private static final boolean USE_WEBCAM = true; // true for webcam, false for phone camera
+    private AprilTagProcessor aprilTag; //the variable to store our instance of the AprilTag processor.
+    private VisionPortal visionPortal; //the variable to store our instance of the vision portal.
+
+    @Override
+    public void runOpMode() {
+        initAprilTag(); // Wait for the DS start button to be touched.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                telemetryAprilTag();
+                telemetry.update(); // Push telemetry to the Driver Station.
+                if (gamepad1.dpad_down) { // Save CPU resources; can resume streaming when needed.
+                    visionPortal.stopStreaming();
+                } else if (gamepad1.dpad_up) {
+                    visionPortal.resumeStreaming();
+                }
+                sleep(20); // Share the CPU.
+            }
+        }
+        visionPortal.close(); // Save more CPU resources when camera is no longer needed.
+    }   // end method runOpMode()
+
+    private void initAprilTag() {
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults(); // Create the AprilTag processor the easy way.
+
+        if (USE_WEBCAM) { // Create the vision portal the easy way.
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTag);
+        } else {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    BuiltinCameraDirection.BACK, aprilTag);
+        }
+    }   // end method initAprilTag()
+
+    private void telemetryAprilTag() { //Function to add telemetry about AprilTag detections
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        for (AprilTagDetection detection : currentDetections) { // Step through the list of detections and display info for each one.
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   // end for() loop
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+    }   // end method telemetryAprilTag()
+}   // end class
